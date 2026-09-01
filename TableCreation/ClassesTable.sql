2e27c7592c7c98f206db489fd9c0ff788b54a5fb
jaredvdb <jared.vandenbosch@gmail.com>
2026-09-01 12:57:19 -0400

    Add default value for section in Classes table
    Added section to enable multiple classes of one course in a semester for students to attend at 'different' times. Aka two students may have the same course in the same semester, but aren't in the same section/class.

--- TableCreation/ClassesTable.sql
+++ TableCreation/ClassesTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS Classes (
     class_id INTEGER PRIMARY KEY CHECK(class_id > 0),
     course_id INTEGER NOT NULL,
+    section INTEGER NOT NULL DEFAULT 1,
     professor_id INTEGER NOT NULL,
     semester TEXT NOT NULL,
     year INTEGER NOT NULL,
