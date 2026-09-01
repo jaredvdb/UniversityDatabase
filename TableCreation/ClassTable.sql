dcd87a08cb3101b0e4427274fb53890842229944
jaredvdb <jared.vandenbosch@gmail.com>
2026-09-01 12:41:01 -0400

    Create Classes table with constraints and foreign keys

---
+++ TableCreation/ClassTable.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS Classes (
+    class_id INTEGER PRIMARY KEY CHECK(class_id > 0),
+    course_id INTEGER NOT NULL,
+    professor_id INTEGER NOT NULL,
+    semester TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    class_start DATE NOT NULL,
+    class_end DATE NOT NULL,
+
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+    FOREIGN KEY (professor_id) REFERENCES Employees(id),
+
+    CHECK(class_end > class_start)
+);
