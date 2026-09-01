7e13f13c0174d32643f007ea34600084a0632cc2
jaredvdb <jared.vandenbosch@gmail.com>
2026-09-01 12:03:09 -0400

    Create Employee table with relevant fields

---
+++ TableCreation/EmployeeTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Employee (
+	id INTEGER PRIMARY KEY CHECK(id>0),
+	name TEXT NOT NULL,
+  field TEXT NOT NULL,
+	dob DATE,
+	date_employed DATE NOT NULL CHECK(date_employed>'2000-08-01') -- Make sure date_employed is after our imaginary schools founding date
+)
