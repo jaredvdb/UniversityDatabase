c25d597f05bd518720f8bc8bc3808c320fb8ba0e
jaredvdb <jared.vandenbosch@gmail.com>
2026-09-01 12:16:34 -0400

    Modify Employee table structure and constraints

--- TableCreation/EmployeeTable.sql
+++ TableCreation/EmployeeTable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS Employee (
 	id INTEGER PRIMARY KEY CHECK(id>0),
 	name TEXT NOT NULL,
-  	field TEXT NOT NULL,
+  	department TEXT NOT NULL,
 	dob DATE,
-	date_employed DATE NOT NULL CHECK(date_employed>'2000-08-01') -- Make sure date_employed is after our imaginary schools founding date
+	date_employed DATE NOT NULL CHECK(date_employed>'2000-08-01'), -- Make sure date_employed is after our imaginary schools founding date
+	professor BOOL NOT NULL -- Checks if teacher or other type of employee (i.e. admin)
 )
