fa826f7f3d3f126c112f18d553475c35d55b9caa
jaredvdb <jared.vandenbosch@gmail.com>
2026-09-02 15:07:55 -0400

    Add output example for student count query
    Added output example for Fall 2025 classes.

--- 02-SelectionQueries/StudentCountByCourseAndSection.sql
+++ 02-SelectionQueries/StudentCountByCourseAndSection.sql
@@ -8,3 +8,21 @@ JOIN Courses as c
 	ON c.course_id = cl.course_id
 WHERE cl.year = 2025 AND cl.semester LIKE 'Fall'
 GROUP BY c.course_code, cl.section;
+
+/* Output (as of commit) for Fall 2025 classes
+course_code	section	studentcount
+"BIO101"	1		5
+"BUS410"	1		6
+"CSC101"	1		3
+"CSC101"	2		2
+"CSC201"	1		3
+"CSC420"	1		2
+"ENG320"	1		5
+"FIN415"	1		5
+"ISC101"	1		3
+"ISC315"	1		5
+"MAT201"	1		3
+"MAT310"	1		2
+"PSY101"	1		3
+"PSY340"	1		3
+*/
